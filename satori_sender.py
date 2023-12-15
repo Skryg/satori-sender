8b9a98b01b31591913889817825baf759cd46f2f
Oskar Krygier <oskar.krygier@enterit.pl>
2023-12-15 19:08:02 +0100

    Handle some exceptions

--- satori_sender.py
+++ satori_sender.py
@@ -1,4 +1,4 @@
-from data import login, password, test_file, problem
+from data import login, password
 import requests
 import argparse
 import time
@@ -12,41 +12,67 @@ parser = argparse.ArgumentParser(
                     prog='Satori Sender',
                     description='Sends specified file to satori. Login credentials need to be specified in data.py, also default file and problem link.')
 
-parser.add_argument('-f', '--file', default=test_file)     # file name
-parser.add_argument('-p', '--problem', default=problem)  # link to the problem
+parser.add_argument('-f', '--file', required=True)     # file name
+parser.add_argument('-p', '--problem', required=True)  # link to the problem
 args = parser.parse_args()
 
 parts = args.problem.split('/')
+
+contestId = None
+problemId = None
+    
 for i in range(len(parts)-1):
     if parts[i] == "contest":
         contestId = parts[i+1]
     if parts[i] == "problems":
         problemId = parts[i+1]
 
+if contestId is None or problemId is None:
+    print("Error: The problem link is not correct or not specified")
+    exit(1)
+
 loginUrl = "https://satori.tcs.uj.edu.pl/login"
 submitUrl = f"https://satori.tcs.uj.edu.pl/contest/{contestId}/submit"
 payload = {'login': login, 'password': password}
 
+try:
+    file = open(args.file,"rb")
+except OSError:
+    print("Error: Given file could not be opened")
+    exit(1)
 
 with requests.Session() as s:
     adapter = HTTPAdapter(max_retries=Retry(total=10, backoff_factor=0.5))
     s.mount('https://', adapter)
-    s.mount('http://', adapter)
+
     login = s.post(loginUrl, data=payload)
     print(f'Login status: {login.status_code}')
     print("Sending a submit")
-    submit = s.post(submitUrl, data={'problem': problemId}, files={'codefile':open(args.file,"rb")})
+    submit = s.post(submitUrl, data={'problem': problemId}, files={'codefile': file})
+    file.close()
+
+
     print("Obtaining submit ID...")
-    
+
     submitID = re.search(r'results/(.*?)"', submit.text).group(1)
+    if submitID == None: 
+        print("Error: could not obtain submit ID, exiting...")
+        exit(1)
+    
     print(f"Submit ID: {Style.BRIGHT}{Fore.CYAN}{submitID}{Style.RESET_ALL}")
     resultsURL = f"https://satori.tcs.uj.edu.pl/contest/{contestId}/results/{submitID}"
     print(f"Waiting for result... {Style.DIM}{Back.YELLOW}QUE{Style.RESET_ALL} now")
     while True:
         time.sleep(5)
-        res = s.get(resultsURL)
-        status = re.search(r'<td class=sta(.*?)>', res.text).group(1)
-        if status != "QUE":
+        try:
+            res = s.get(resultsURL)
+            status = re.search(r'<td class=sta(.*?)>', res.text).group(1)
+        except:
+            print("Something went wrong")
+            print(resultsURL)
+            exit(1)
+
+        if status != "QUE" and status != "None":
             if status == "OK":
                 print(f"Status: {Back.GREEN}{status}{Style.RESET_ALL} :D")
             else:
@@ -54,3 +80,4 @@ with requests.Session() as s:
                 
             print(resultsURL)
             break
+            
